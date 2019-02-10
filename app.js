7e1b5b10f058034a7aeec934924c179d9c95830d
Chad <chadhill2.0@gmail.com>
2019-02-10 15:19:35 -0800

    forgot to make new branch before changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function sum(a, b) { //eslint-disable-line
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The product of 5 and 9 is 45."
+"The product of 5 and 9 is 45."'The product of ' + a + ' and ' + b + ' is ' + multipleStorage + '.'
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
@@ -57,11 +57,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumArray = [];
+  var sumVar = sum(a,b,c);
+  var productVar = multiply(a,b,c);
+  var thirdElement = (a + ' and ' + b + ' and ' + c + ' sum to ' + sumVar);
+  var fourthElement = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productVar);
+  sumArray.push(sumVar,productVar,thirdElement,fourthElement);
+  return sumArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
